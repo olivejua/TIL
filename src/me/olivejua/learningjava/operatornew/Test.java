a1cee6701a1e6c79de370315104ddf5021d76de2
olivejua <tmfrl4710@gmail.com>
2022-05-15 17:40:02 +0900

    new 연산자 학습

---
+++ src/me/olivejua/learningjava/operatornew/Test.java
@@ -0,0 +1,37 @@
+package me.olivejua.learningjava.operatornew;
+
+// Box class
+class Box
+{
+    double width;
+    double height;
+    double depth;
+}
+
+// Driver class
+public class Test
+{
+
+    // Driver method
+    public static void main(String[] args)
+    {
+        // creating box object
+        Box b1 = new Box();
+
+        // assigning b2 to b1
+        Box b2 = b1;
+
+        // height via b1 and b2
+        System.out.println(b1.height);
+        System.out.println(b2.height);
+
+        // changing height via b2
+        b2.height = 20;
+
+        // height via b1 and b2
+        // after modification through b2
+        System.out.println(b1.height);
+        System.out.println(b2.height);
+    }
+
+}
