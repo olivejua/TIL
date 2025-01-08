4ca11b4faabf1df1136b5e2365a80b13b57dfe4c
seulki <tmfrl4710@gmail.com>
2025-01-09 01:18:53 +0900

    스케줄러 생성

---
+++ scheduler/demo-scheduler/src/main/java/org/example/app/tasks/CustomScheduler.java
@@ -0,0 +1,26 @@
+package org.example.app.tasks;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Slf4j
+@Component
+public class CustomScheduler {
+
+    // 매 1초마다 실행
+    @Scheduled(fixedRate = 1000)
+    public void runTask1() throws InterruptedException {
+        int taskId = (int)(Math.random() * 100);
+        log.info("[task1-{}] Start", taskId);
+        Thread.sleep(5_000);
+        log.info("[task1-{}] End", taskId);
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void runTask2() throws InterruptedException {
+        log.info("[task2] Execute");
+    }
+}
