27fbde76440518a5d413675826d1a4227c83dcf2
seulki <tmfrl4710@gmail.com>
2025-01-13 23:42:00 +0900

    IScheduler 생성

--- scheduler/demo-scheduler/src/main/java/org/example/app/SchedulerConfig.java
+++ scheduler/demo-scheduler/src/main/java/org/example/app/SchedulerConfig.java
@@ -1,9 +1,12 @@
 package org.example.app;
 
+import lombok.extern.slf4j.Slf4j;
+import org.example.app.v1.tasks.V1Scheduler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+@Slf4j
 @Configuration
 public class SchedulerConfig {
 
@@ -12,6 +15,14 @@ public class SchedulerConfig {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(10);
         scheduler.initialize();
+
+        return scheduler;
+    }
+
+    @Bean
+    public IScheduler scheduler() {
+        IScheduler scheduler = new V1Scheduler();
+        log.info(scheduler.getClass().getName());
 
         return scheduler;
     }
