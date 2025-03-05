f3dcc12135761c62f12ba31df7bb7fc840fea39a
olivejua <tmfrl4710@gmail.com>
2025-03-05 21:41:15 +0900

    aop-proxy 프로젝트 생성

---
+++ spring_framework/aop-proxy/src/main/java/com/example/aopproxy/AopProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.aopproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopProxyApplication.class, args);
+    }
+
+}
---
+++ spring_framework/aop-proxy/src/test/java/com/example/aopproxy/AopProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aopproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
